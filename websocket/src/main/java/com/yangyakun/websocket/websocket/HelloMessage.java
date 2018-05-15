d0adbce9c1fe13bfd2b767385c5025dc3839fece
yangyakun <yangyakun921@gmail.com>
2018-05-16 00:43:50 +0800

    websockert

---
+++ websocket/src/main/java/com/yangyakun/websocket/websocket/HelloMessage.java
@@ -0,0 +1,21 @@
+package com.yangyakun.websocket.websocket;
+
+public class HelloMessage {
+
+    private String name;
+
+    public HelloMessage() {
+    }
+
+    public HelloMessage(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
